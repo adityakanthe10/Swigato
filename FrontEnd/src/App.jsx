abdf3ca0f63b20ed7fcf8154310ea7f4bad0c7d2
Aditya Kanthe <adityakanthe10@gmail.com>
2024-05-07 19:02:34 +0530

    created website responsive and login ,sign up page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopUp/LoginPopUp'
+
 const App = () => {
+
+const[showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
     <div className="app">
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>}/>
---
+++ FrontEnd/src/components/LoginPopUp/LoginPopUp.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import './LoginPopUp.css'
+import {assets} from '../../assets/assets'
+
+const LoginPopUp = ({setShowLogin}) => {
+
+const [currState,setCurrState] = useState("Login")
+
+  return (
+    <div className="login-popup">
+        <form className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-input">
+                {currState==='Login'?<></>: <input type="text" placeholder='Your name' required />}
+                <input type="email" placeholder='Your email' required />
+                <input type="password" placeholder='password' required />
+            </div>
+            <button>{currState==='Sign Up'?"Create account":"Login"}</button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p>By continuing, i agree to the terms of use & privacy policy</p>
+            </div>
+            {currState==="Login"
+            ? <p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+            :<p>Already have an Account?<span onClick={()=>setCurrState("Login")}>Login here</span></p>
+        }
+        </form>
+    </div>
+  )
+}
+
+export default LoginPopUp
