e7a74f11371456dc81d6292e81b24f0cf43c3b4e
Aditya Kanthe <adityakanthe10@gmail.com>
2024-05-07 18:00:13 +0530

    Created Frontend Food Delivery Website

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
+    <>
     <div className="app">
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>} />
+        </Routes>
     </div>
+    <Footer />
+    </>
   )
 }
 
